341dd635903a273f75927698dff81f7bd02c2ba2
suelense <suelense@gmail.com>
2024-04-22 14:28:50 -0300

    alterações de formatação e script

--- README-template.md
+++
@@ -1,4 +0,0 @@
-Users should be able to:
-
-
-- **Bonus**: See the age numbers animate to their final number when the form is submitted
--- script.js
+++ script.js
@@ -1,94 +1,109 @@
-function validarVazio(campo) {
-    var data = document.getElementById(campo);
-    if (data.value == "") {
-        document.getElementsByClassName("erro_vazio_" + campo)[0].style.display = "block";
-        document.getElementById(campo).style.borderColor = "#ff5757";
-        document.getElementById("label_" + campo).style.color = "#ff5757";
+function getValue(id) {
+    return parseInt(document.getElementById(id).value)
+}
+
+function changeAttribute(className, attribute, value) {
+    document.getElementsByClassName(className)[0].style[attribute] = value
+}
+
+function displayBlockAndRed(id) {
+    changeAttribute("error_empty_" + id, "display", "block");
+    changeAttribute(id, "borderColor", "#ff5757");
+    changeAttribute("label_" + id, "color","#ff5757");
+}
+
+function displayNoneAndGray(id) {
+    changeAttribute("error_empty_" + id, "display", "none");
+    changeAttribute(id, "borderColor", "#716f6f");
+    changeAttribute("label_" + id, "color", "#716f6f");
+}
+
+function validateEmpty(id) {
+    var date = getValue(id);
+    if (isNaN(date)) {
+        displayBlockAndRed(id);
     }
     else {
-        document.getElementsByClassName("erro_vazio_" + campo)[0].style.display = "none";
-        document.getElementById(campo).style.borderColor = "#716f6f";
-        document.getElementById("label_" + campo).style.color = "#716f6f";
+        displayNoneAndGray(id);
+        return true;
     }
 }
 
-function validarMes31Dias(dia) {
-    if (dia < 0 || dia > 31) {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "block";
-        document.getElementById("dia").style.borderColor = "#ff5757";
-        document.getElementById("label_dia").style.color = "#ff5757";
+function validate31Days(day) {
+    if (day < 0 || day > 31) {
+        document.getElementsByClassName("error_validate_day")[0].style.display = "block";
+        document.getElementById("day").style.borderColor = "#ff5757";
+        document.getElementById("label_day").style.color = "#ff5757";
     } else {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "none";
+        document.getElementsByClassName("error_validate_day")[0].style.display = "none";
     }
 }
 
-function validarMes30Dias(dia) {
-    if (dia < 0 || dia > 30) {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "block";
-        document.getElementById("dia").style.borderColor = "#ff5757";
-        document.getElementById("label_dia").style.color = "#ff5757";
+function validate30Days(day) {
+    if (day < 0 || day > 30) {
+        document.getElementsByClassName("error_validate_day")[0].style.display = "block";
+        document.getElementById("day").style.borderColor = "#ff5757";
+        document.getElementById("label_day").style.color = "#ff5757";
     } else {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "none";
+        document.getElementsByClassName("error_validate_day")[0].style.display = "none";
     }
 
 }
 
-function validarMesFevereiro(dia) {
-    if (dia < 0 || dia > 29) {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "block";
-        document.getElementById("dia").style.borderColor = "#ff5757";
-        document.getElementById("label_dia").style.color = "#ff5757";
+function validateFebruary(day) {
+    if (day < 0 || day > 29) {
+        document.getElementsByClassName("error_validate_day")[0].style.display = "block";
+        document.getElementById("day").style.borderColor = "#ff5757";
+        document.getElementById("label_day").style.color = "#ff5757";
     } else {
-        document.getElementsByClassName("erro_validar_dia")[0].style.display = "none";
+        document.getElementsByClassName("error_validate_day")[0].style.display = "none";
     }
 }
 
-function validarDia() {
-    var dia = parseInt(document.getElementById("dia").value);
-    var mes = parseInt(document.getElementById("mes").value);
-    var meses31 = [1, 3, 5, 7, 8, 10, 12];
-    if (meses31.includes(mes)) {
-        validarMes31Dias(dia);
-    } else if (mes == 2) {
-        validarMesFevereiro(dia);
+function validateDay() {
+    var day = getValue("day");
+    var month = getValue("month");
+    var months31 = [1, 3, 5, 7, 8, 10, 12];
+    if (months31.includes(month)) {
+        validate31Days(day);
+    } else if (month == 2) {
+        validateFebruary(day);
     } else {
-        validarMes30Dias(dia);
+        validate30Days(day);
     }
 }
 
-function validarMes() {
-    var mes = document.getElementById("mes");
-    if (mes.value < 0 || mes.value > 12) {
-        document.getElementsByClassName("erro_validar_mes")[0].style.display = "block";
-        document.getElementById("mes").style.borderColor = "#ff5757";
-        document.getElementById("label_mes").style.color = "#ff5757";
+function validateMonth() {
+    var month = getValue("month");
+    if (month < 0 || month > 12) {
+        document.getElementsByClassName("error_validate_month")[0].style.display = "block";
+        document.getElementById("month").style.borderColor = "#ff5757";
+        document.getElementById("label_month").style.color = "#ff5757";
     } else {
-        document.getElementsByClassName("erro_validar_mes")[0].style.display = "none";
+        document.getElementsByClassName("error_validate_month")[0].style.display = "none";
     }
 }
 
-function validarAnoFuturo() {
-    var dataAtual = new Date();
-    var anoAtual = dataAtual.getFullYear();
-    var ano = document.getElementById("ano");
-    if (ano.value > anoAtual) {
-        document.getElementsByClassName("erro_validar_ano")[0].style.display = "block";
+function validateFutureYear() {
+    var actualDate = new Date();
+    var actualYear = actualDate.getFullYear();
+    var year = getValue("year");
+    if (year > actualYear) {
+        document.getElementsByClassName("error_validate_year")[0].style.display = "block";
     } else {
-        document.getElementsByClassName("erro_validar_ano")[0].style.display = "none";
+        document.getElementsByClassName("error_validate_year")[0].style.display = "none";
     }
 }
 
-
-
-function validarData() {
-    validarVazio("dia");
-    validarVazio("mes");
-    validarVazio("ano");
-    validarDia();
-    validarMes();
-    validarAnoFuturo();
+function validateDate() {
+    validateEmpty("day");
+    validateEmpty("month");
+    validateEmpty("year");
+    validateDay();
+    validateMonth();
+    validateFutureYear();
 }
 
-function calcularIdade() {
-    validarData();
+function calculateAge() {
+    validateDate();
 }
